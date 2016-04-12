f54d1c42ce81575e5e4736734d43076c4fa65a3f
RafaelZavalagit <rafael_zalo7@hotmail.com>
2016-04-12 11:00:18 -0600

    Archivo JS, funcion anonima y consola

---
+++ javascript/js/codigo.js
@@ -0,0 +1,13 @@
+var botonClic=function ()
+{
+	alert("Click del boton");
+}
+
+var nombre="Martin";
+nombre="Rafael";
+document.write(" <strong> JS en accion</strong> "+"  "+nombre);
+
+console.log("A punto de lanzar un mensaje");
+
+alert("Mensaje por pantalla");
+
