cac7e1b4110784beb80a218acee2e9a96be96150
RafaelZavalagit <rafael_zalo7@hotmail.com>
2016-04-14 10:56:54 -0600

    Eventos principales de calculadora, falta borrar

---
+++ Calculadora/js/calculadora.js
@@ -0,0 +1,37 @@
+//variable global
+var operador="";
+function numeros(num)
+{
+	var valor=document.calculadora.operando1.value;
+	if(operador=="") //escribir en el operando1
+	{
+		if(valor=="0") //vaciamos la caja
+		{
+			document.calculadora.operando1.value="";	
+		}
+		document.calculadora.operando1.value=
+		document.calculadora.operando1 +num;
+	}
+	else //escribir en el operando2
+	{
+		if(valor=="0") //vaciamos la caja
+		{
+			document.calculadora.operando2.value="";	
+		}
+		document.calculadora.operando2.value=
+		document.calculadora.operando2.value +num;
+	}          
+}
+function operadores(ope)
+{
+	alert(ope);
+}
+function igual()
+{
+	var valor1=document.calculadora.operando1.value;
+	var valor2=document.calculadora.operando2.value;
+	var resultado;
+	operador=document.calculadora.operador.value=
+	eval(operando1+operador+operando2);	
+	//eval es una funcion que evaula cualquier operacion matematica
+}
