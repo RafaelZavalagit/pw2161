eaad3a0bf91a609929e7405c4f24ede778018c92
RafaelZavalagit <rafael_zalo7@hotmail.com>
2016-04-26 11:02:58 -0600

    Gatos terminados

--- Gato/js/gato.js
+++ Gato/js/gato.js
@@ -9,6 +9,22 @@ var cuentaJugadas=0;
 function iniciaGato()
 {
 	//En construccion 
+	//preguntar si el navegador 
+	if(typeof(Storage) != "undefined")  //Para preguntar si el navegador es compatible con "localStorage"
+	{
+		if(localStorage.webCuentaJuego)
+		{
+			cuentaJuego=localStorage.webCuentaJuego;
+			cuentaJuego++;
+		}
+		else
+		{
+			cuentaJuego=1;
+		}
+		document.getElementById("tituloJuego").innerHTML="Juego del Gato (#"+cuentaJuego+")";
+	}
+	else
+		alert("Utiliza un navegador actualizado!");
 }
 function escribe(casilla)
 {
@@ -28,8 +44,11 @@ function escribe(casilla)
 		turno=!turno;
 	}
 	cuentaJugadas++; //para saber quien gano, validamos la jugada
+	
 	validaJugada(letra);
 }
+
+
 function validaJugada(valor)
 {
 	var ganador=false;
@@ -42,29 +61,41 @@ function validaJugada(valor)
 	var b31=document.getElementById("tresuno").innerHTML;
 	var b32=document.getElementById("tresdos").innerHTML;
 	var b33=document.getElementById("trestres").innerHTML;
-
-	if(b11==b12 &&b12==b13&& b13!="&nbsp;")
-		ganador=true;
-	if(b11==b22 &&b22==b23&& b23!="&nbsp;")
-		ganador=true;
-	if(b11==b32 &&b12==b33&& b33!="&nbsp;")
-		ganador=true;
-	if(b11==b21 &&b21==b31&& b31!="&nbsp;")
-		ganador=true;
-	if(b12==b22 &&b22==b32&& b32!="&nbsp;")
-		ganador=true;
-	if(b13==b23 &&b13==bb33&& b33!="&nbsp;")
-		ganador=true;
-	if(b11==b22 &&b22==b33&& b33!="&nbsp;")
-		ganador=true;
-	if(b13==b22 &&b22==b31&& b13!="&nbsp;")
-		ganador=true;
-	
+	if(cuentaJugadas>=4)
+	{
+		if(b11==b12 &&b12==b13&& b13!="&nbsp;")
+			ganador=true;
+		if(b21==b22 &&b22==b23&& b23!="&nbsp;")
+			ganador=true;
+		if(b31==b32 &&b32==b33&& b33!="&nbsp;")
+			ganador=true;
+		if(b11==b21 &&b21==b31&& b31!="&nbsp;")
+			ganador=true;
+		if(b12==b22 &&b22==b32&& b32!="&nbsp;")
+			ganador=true;
+		if(b13==b23 &&b13==b33&& b33!="&nbsp;")
+			ganador=true;
+		if(b11==b22 &&b22==b33&& b33!="&nbsp;")
+			ganador=true;
+		if(b13==b22 &&b22==b31&& b13!="&nbsp;")
+			ganador=true;
+	}
+		
+		
 
 	if(ganador==true)
 	{
-		alert("Ganador: "+letra);
+		alert("Ganador: "+valor);
+		localStorage.webCuentaJuego=cuentaJuego;  //Para eliminar la variable del localStorage: localStorage.removeItem("webCuentaJuego")
+		location.reload();
+
 	}else if(ganador==false && cuentaJugadas==9){
 		alert("Empate!!!");
+		localStorage.webCuentaJuego=cuentaJuego;
+		location.reload();
 	}
+}
+function Recargar()
+{
+	location.reload();
 }
--- equipopw/js/gato.js
+++ equipopw/js/gato.js
@@ -20,6 +20,27 @@ function seleccion(valor,id)
 
 		
 }
+function iniciaGato()
+{
+	//En construccion 
+	//preguntar si el navegador 
+	if(typeof(Storage) != "undefined")  //Para preguntar si el navegador es compatible con "localStorage"
+	{
+		if(localStorage.webCuentaJuego)
+		{
+			cuentajuegos=localStorage.webCuentaJuego;
+			cuentajuegos++;
+		}
+		else
+		{
+			cuentajuegos=1;
+		}
+		document.getElementById("tituloJuego").innerHTML="Juego del Gato (#"+cuentajuegos+")";
+	}
+	else
+		alert("Utiliza un navegador actualizado!");
+}
+
 function ganadores(id1,id2,id3) //  para cambiar los colores de los botones de ganadores ganadores('s11','s12','s13');
 {
 	document.getElementById(id1).style.background=red;
@@ -34,6 +55,7 @@ function checar()
 		if((document.getElementById('s11').value==document.getElementById('s12').value)&&(document.getElementById('s12').value==document.getElementById('s13').value)&&document.getElementById('s11').value!=""&&ganador==false)
 		{
 			alert("El jugador con "+document.getElementById('s11').value+" es el Ganador");
+			localStorage.webCuentaJuego=cuentajuegos; 
 			location.reload();
 			ganador=true;
 			contador=0;
@@ -43,6 +65,8 @@ function checar()
 		if((document.getElementById('s21').value==document.getElementById('s22').value)&&(document.getElementById('s22').value==document.getElementById('s23').value)&&document.getElementById('s21').value!=""&&ganador==false)
 		{
 			alert("El jugador con "+document.getElementById('s21').value+" es el Ganador");
+			localStorage.webCuentaJuego=cuentajuegos; 
+
 			location.reload();
 			ganador=true;
 			contador=0;
@@ -51,6 +75,7 @@ function checar()
 		if((document.getElementById('s31').value==document.getElementById('s32').value)&&(document.getElementById('s32').value==document.getElementById('s33').value)&&document.getElementById('s31').value!=""&&ganador==false)
 		{
 			alert("El jugador con "+document.getElementById('s31').value+" es el Ganador");
+			localStorage.webCuentaJuego=cuentajuegos; 
 			location.reload();
 			ganador=true;
 			contador=0;
@@ -59,6 +84,7 @@ function checar()
 		if((document.getElementById('s11').value==document.getElementById('s21').value)&&(document.getElementById('s21').value==document.getElementById('s31').value)&&document.getElementById('s11').value!=""&&ganador==false)
 		{
 			alert("El jugador con "+document.getElementById('s11').value+" es el Ganador");
+			localStorage.webCuentaJuego=cuentajuegos; 
 			location.reload();
 			ganador=true;
 			contador=0;
@@ -67,6 +93,7 @@ function checar()
 		if((document.getElementById('s12').value==document.getElementById('s22').value)&&(document.getElementById('s22').value==document.getElementById('s32').value)&&document.getElementById('s22').value!=""&&ganador==false)
 		{
 			alert("El jugador con "+document.getElementById('s22').value+" es el Ganador");
+			localStorage.webCuentaJuego=cuentajuegos; 
 			location.reload();
 			ganador=true;
 			contador=0;
@@ -75,6 +102,7 @@ function checar()
 		if((document.getElementById('s13').value==document.getElementById('s23').value)&&(document.getElementById('s23').value==document.getElementById('s33').value)&&document.getElementById('s33').value!=""&&ganador==false)
 		{
 			alert("El jugador con "+document.getElementById('s33').value+" es el Ganador");
+			localStorage.webCuentaJuego=cuentajuegos; 
 			location.reload();
 			ganador=true;
 			contador=0;
@@ -83,6 +111,7 @@ function checar()
 		if((document.getElementById('s11').value==document.getElementById('s22').value)&&(document.getElementById('s22').value==document.getElementById('s33').value)&&document.getElementById('s11').value!=""&&ganador==false)
 		{
 			alert("El jugador con "+document.getElementById('s11').value+" es el Ganador");
+			localStorage.webCuentaJuego=cuentajuegos; 
 			location.reload();
 			ganador=true;
 			contador=0;
@@ -91,6 +120,7 @@ function checar()
 		if((document.getElementById('s31').value==document.getElementById('s22').value)&&(document.getElementById('s22').value==document.getElementById('s13').value)&&document.getElementById('s13').value!=""&&ganador==false)
 		{
 			alert("El jugador con "+document.getElementById('s13').value+" es el Ganador");
+			localStorage.webCuentaJuego=cuentajuegos; 
 			location.reload();
 			ganador=true;
 			contador=0;
@@ -100,6 +130,7 @@ function checar()
 	}
 	if(contador==10){
 		alert("No hay ganador!! ")
+		localStorage.webCuentaJuego=cuentajuegos; 
 		location.reload();
 	}
 		
