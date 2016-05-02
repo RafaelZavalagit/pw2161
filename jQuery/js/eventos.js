3e24e3389988de8fdbdf8fe294bc6f6a11d9b5e5
RafaelZavalagit <rafael_zalo7@hotmail.com>
2016-05-02 11:03:23 -0600

    Teclas input en el JQuery

--- jQuery/js/eventos.js
+++ jQuery/js/eventos.js
@@ -6,9 +6,28 @@ var inicio=function()
 		console.log("click del boton!!");
 		$(".anuncioWeb").append("Click del Boton");
 	}
-	$("#miBoton").on("click",clicBoton);
+	var clicBoton2 =function()
+	{
+		alert("Boton 2");
+	}	
+	var teclaunInput = function(tecla)
+	{
+		if(tecla.which == 13)
+		{
+
+			//que se posicione en el segundo input.
+			$("#otroInput").focus();
+		}
+	}
+
+	
+	$("#miBoton").off("click",clicBoton);
+	$("#miBoton").on("click",clicBoton2);
+$("#unInput").on("keypress",teclaunInput);
 	
 
 }
+
 //Main
 $ (document).on("ready",inicio);
+
