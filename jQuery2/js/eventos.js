4ae059410fe87f02b0098fe1e99fbf97d93212a6
RafaelZavalagit <rafael_zalo7@hotmail.com>
2016-05-24 11:04:05 -0600

    Generando eventos dinamicos en consultas

--- jQuery2/js/eventos.js
+++ jQuery2/js/eventos.js
@@ -64,22 +64,32 @@ var iniciaApp = function()
 
 	var Altas = function(){
 		//Mostramos el formulario
-		$("altaUsuarios").show("slow");
+		$("#altaUsuarios").show("slow");
+		$("#altaUsuarios h2").html("Alta Usuarios");
 
+		//apago la funcion de alta de usuarios
+		$("frmAltaUsuarios").off("submit",BajaUsuarios);  //(Aqui se apaga la funcion de Bajas en ese boton)
+	
 
+		//Enciendo la funcion de Baja Usuario 
+		//en el mismo boton (el submit)
+		
+		$("#frmAltaUsuarios").on("submit",AltaUsuarios);  	 //(Aqui prende el boiton de Alta)
 	}
 
 	var AltaUsuarios = function ()
 	{
 		$("btnAltaUsuario").on("submit",AltaUsuarios);
 		//alert($("#frmAltaUsuarios").serialize());
+		
+
 		var datos=$("#frmAltaUsuarios").serialize();
 		var parametros ="accion=guardaUsuario&"+datos+"&id="+Math.random();   //importante, el & despues de GuardaUsuarios
 
-		$ajax({
+		$.ajax({
 			beforeSend:function(){
 				//Validar al usuario
-				console.log("Validar al usuario");
+				console.log("Alta usuario");
 			},
 			cache: false,
 			type: "POST",
@@ -101,12 +111,155 @@ var iniciaApp = function()
 	
 	}
 
+	var Bajas = function()
+	{
+		$("#altaUsuarios").show("slow");
+		$("#altaUsuarios h2").html("Baja de Usuarios");  //Asi podemos referenciar a un objeto del index.html
+		//apago la funcion de alta de usuarios
+		$("#frmAltaUsuarios".off("submit",AltaUsuarios));
+		//Enciendo la funcion de Baja Usuario 
+		//en el mismo boton (el submit)
+		$("#frmAltaUsuarios").on("submit",BajaUsuarios);
+	}
 
+/*
++	var BajaUsuario = function()
++	{
++		event.preventDefault();
++		//var datos = $("#frmAltaUsuarios").serialize();
++		var datos = "txtNombreUsuario="+$("#txtNombreUsuario").val();
++		var parametros = "accion=bajaUsuario&"+datos+
++		                 "&id="+Math.random();
++		$.ajax({
++			beforeSend:function(){
++				console.log("Baja al usuario");
++			},
++			cache: false,
++			type: "POST",
++			dataType: "json",
++			url:"php/funciones.php",
++			data:parametros,
++			success: function(response){
++				if(response.respuesta == true )//¬¬
++				{
++					alert("Usuario dado de baja correctamente");
++				}
++				else
++				{
++					alert("No se pudo dar de baja la información");
++				}
++			},
++			error: function(xhr,ajax,thrownError){
++
++			}
++		});
++	} */
+
+	var BajaUsuarios = function () 
+	{
+		event.preventDefault();
+
+		//copiamos todo lo de AltaUsuarios
+		//var datos=$("#frmAltaUsuarios").serialize();
+
+		var datos= "txtNombreUsuario="+$("#txtNombreUsuario").val();
+		var parametros ="accion=BajaUsuarios&"+datos+"&id="+Math.random();   //importante, el & despues de GuardaUsuarios
+
+		$ajax({
+			beforeSend:function(){
+				//Validar al usuario
+				console.log("Baja usuario");
+			},
+			cache: false,
+			type: "POST",
+			dataType:"json",
+			url: "php/funciones.php",
+			data: parametros,
+			success: function(response){
+				if(response.respuesta == true){
+					alert("Usuario dado de Baja Correctamente");
+				}
+				else{
+					alert("No se pudo guardar la informacion")
+				}
+			},
+			error: function(xhr,ajaxOptions,thrownError){
+				console.log("Hay errores en php!");
+			}
+		});
+	}
+
+	var Consultas = function()
+	{
+		$("#consultasUsuarios").show("slow");
+		var parametros  = "accion=consultas"+"&id="+Math.random;
+
+		$.ajax({
+			beforeSend:function(){
+				//Validar al usuario
+				console.log("Consulta de usuario");
+			},
+			cache: false,
+			type: "POST",
+			dataType:"json",
+			url: "php/funciones.php",
+			data: parametros,
+
+			success: function(response){
+				//console.log(response.log);
+				if(response.respuesta == true){
+					$("#tablaConsultas").html(response.tabla);
+					//$("#tablaConsultas").append(response.tablaConsultas);  //Esto del html en "" es para limpiar la tabla, con el append se refrescaria la pagina
+
+				}
+			},
+			error: function(xhr,ajaxOptions,thrownError){
+				console.log("Ha ocurrido un error");
+			}
+
+		});
+	}
+
+	var BajaDinamica = function()
+	{
+		var usuario = $(this).attr(id);
+		alert(usuario);
+		//con el This se viene el objeto al q dimos click
+		var parametros ="accion=BajaUsuarios("+usuario+")&"+"&id="+Math.random();   //importante, el & despues de GuardaUsuarios
+
+		$ajax({
+			beforeSend:function(){
+				//Validar al usuario
+				console.log("Baja dinamica de usuario");
+			},
+			cache: false,
+			type: "POST",
+			dataType:"json",
+			url: "php/funciones.php",
+			data: parametros,
+			success: function(response){
+				if(response.respuesta == true){
+					alert("Usuario dado de Baja Correctamente");
+				}
+				else{
+					alert("No se pudo guardar la informacion")
+				}
+			},
+			error: function(xhr,ajaxOptions,thrownError){
+				console.log("Hay errores en php!");
+			}
+		});
+		
+	}
 	$("#frmValidaEntrada").on("submit",validaEntrada);
+	$("#btnAltas").on("click",Altas);
+	$("#frmAltaUsuarios").on("submit",AltaUsuarios);
+	$("#btnBajas").on("click",Bajas);
+	$("#btnConsultas").on("click",Consultas);
 
-	$("#btnAltas").on("ready",Altas);
-	$("frmAltaUsuarios").on("submit",AltaUsuarios);
-
-
+	//Eventos Dinamicos
+	$("#tablaConsultas").on("button","click",BajaDinamica);
+	//otra forma
+	//$("#tablaConsultas > input").on("click",BajaDinamica);
 }
 $(document).on("ready",iniciaApp);
--- jQuery2/php/funciones.php
+++ jQuery2/php/funciones.php
@@ -72,8 +72,76 @@ function guardaUsuario(){
 	if(mysql_affected_rows() > 0){
 		$respuesta=true;
 	}
-	$salidaJSON = array('respuesta' =>, $respuesta);
+	$salidaJSON = array('respuesta' => $respuesta);
 	print json_encode($salidaJSON);
+}
+
+function bajaUsuarios(){
+	$respuesta= false;
+	$usuario = GetSQLValueString($_POST["txtNombreUsuario"],"text");
+	mysql_connect("localhost","root","");
+	mysql_select_db("cursopw");
+
+	$consultaBaja= sprintf("delete from usuarios where usuario=%s limit 1",$usuario);
+
+	// $conbsultaBaja= sprintf("update from usuarios set tipousuario='baja' where usuario=%s",$usuarios);
+	mysql_query($consultaBaja);
+	if(mysql_affected_rows() > 0) {
+		$respuesta=true;
+	}
+		
+	$salidaJSON = array ('respuesta' => $respuesta);
+	print json_encode($salidaJSON);
+
+}
+function BajaDinamica($usuario){
+	$respuesta=false;
+	mysql_connect("localhost","root","");
+	mysql_select_db("cursopw");
+
+	$consultaBaja= sprintf("delete from usuarios where usuario=%s limit 1",$usuario);
+
+	// $conbsultaBaja= sprintf("update from usuarios set tipousuario='baja' where usuario=%s",$usuarios);
+	mysql_query($consultaBaja);
+	if(mysql_affected_rows() > 0) {
+		$respuesta=true;
+	}
+		
+	$salidaJSON = array ('respuesta' => $respuesta);
+	print json_encode($salidaJSON);
+}
+function consultas(){
+	$respuesta=false;
+	mysql_connect("localhost","root","");
+	mysql_select_db("cursopw");
+
+	$consulta ="select * from usuarios order by usuario";
+	
+	$resultado=mysql_query($consulta);
+	$tabla = "";
+
+	if(mysql_num_rows($resultado) > 0)
+	{
+		$respuesta = true;
+		$tabla.= "<tr>";
+		$tabla.="<th>Usuario</th>";
+		$tabla.="<th>Tipo Usuario</th>";
+		$tabla.="<th>Departamento</th>";
+		$tabla.="<th>Acciones </th>";
+		while( $registro = mysql_fetch_array($resultado))  //en este while el registro va tomando el valor de los objetos de tippo usuario.
+		{
+			$tabla.= "<tr>";
+			$tabla.= "<td>".$registro["usuario"]."<td>";
+			$tabla.= "<td>".$registro["tipousuario"]."<td>";
+			$tabla.= "<td>".$registro["departamento"]."<td>";
+			$tabla.="<td><button id='".$registro["usuario"]."' class='btn btn-danger'>Baja</button></td>";
+			$tabla.="</tr>";
+		}
+	}
+	$salidaJSON = array('respuesta' => $respuesta,
+						'tabla' => $tabla);
+	print json_encode($salidaJSON);
+		
 }
 
 $accion = $_POST["accion"];
@@ -85,6 +153,12 @@ switch ($accion) {
 		break;
 	case 'guardaUsuario':
 		guardaUsuario();
+		break;
+	case 'bajaUsuarios':
+		BajaUsuario();
+		break;
+	case 'consultas':
+		consultas();
 		break;
 	default:
 		# code...
